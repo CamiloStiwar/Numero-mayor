ad3c939b2f6dd649d23e15311b82c1d5a432b5a1
CamiloStiwar <camilo.cm96@gmail.com>
2023-03-02 10:23:01 -0500

    mayor que

---
+++ calculoMayor.py
@@ -0,0 +1,10 @@
+#pedirle al usuario dos valores númericos y vamos a imprimir el mayor de ellos
+
+valor1 = int(input("Por favor digite el primer valor: "))
+valor2 = int(input("Por favor digite el segundo valor: "))
+
+if valor1 > valor2:
+    print("El valor 1 es mayor al valor 2")
+
+else:
+    print("El valor 2 es mayor al valor 1")
